4863d8bbedcfb267de524e935c35c0a08dc9d26f
user <user@DESKTOP-S9IISRV>
2020-04-12 13:13:54 +0900

    Day 02

--- src/step05/Ex01.java
+++ src/step05/Ex01.java
@@ -25,6 +25,7 @@ public class Ex01 {
 		int num = 8;
 		System.out.println(num % 2 == 0);
 		System.out.println(num % 2 == 1);
+
 		if (num % 2 == 0) {
 			System.out.println("짝수");
 		}
@@ -34,19 +35,34 @@ public class Ex01 {
 
 		// 문제 1) 양수, 0, 음수 출력
 		num = -10;
-		if(num>0) {
+		if (num > 0) {
 			System.out.println("양수");
-		}else if(num == 0) {
+		}
+		if (num == 0) {
 			System.out.println("0");
-		}else {
+		}
+		if (num < 0) {
 			System.out.println("음수");
 		}
 
 		// 문제 2) 4의 배수 출력
 		num = 12;
+		if (num % 4 == 0) {
+			System.out.println(num + "는 4의 배수입니다");
+		}
+		if (num % 4 != 0) {
+			System.out.println(num + "는 4의 배수가 아니다");
+
+		}
 
 		// 문제 3) 합격, 불합격 출력
 		int score = 87;
+		if (score >= 60) {
+			System.out.println("합격");
+		}
+		if (score < 60) {
+			System.out.println("불합격");
+		}
 
 	}
 
---
+++ src/step05/Ex02.java
@@ -0,0 +1,30 @@
+package step05;
+
+import java.util.Scanner;
+
+/*
+ * # Up & Down 게임[1단계]
+ * 1. com은 8이다.
+ * 2. me는 com의 숫자를 맞추는 게임이다.
+ * 3. 다음과 같은 메세지를 출력한다.
+ * 1) me < com	: Up!
+ * 2) me == com : Bingo!
+ * 3) me > com  : Down!
+ */
+
+public class Ex02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int com = 8;
+		
+		System.out.println("숫자입력: ");
+		int me = sc.nextInt();
+
+		if (me < com) {System.out.println("UP!");}
+		if (me == com) {System.out.println("BINGO!");}
+		if (me > com) {System.out.println("DOWN!");
+		}
+
+	}
+}
