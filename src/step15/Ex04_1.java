56d3b16f1989972d7afddeb14c43ad9a259f94ee
user <user@DESKTOP-S9IISRV>
2020-04-26 13:02:27 +0900

    Day 05 step16

--- src/step15/Ex04_1.java
+++ src/step15/Ex04_1.java
@@ -10,32 +10,36 @@ import java.util.Scanner;
  * 3. 게임 종료 후, 성적을 출력한다.
  */
 
-public class Ex04 {
+public class Ex04_1 {
 
 	public static void main(String[] args) {
 		Random ran = new Random();
 		Scanner sc = new Scanner(System.in);
 
 		int score = 0;
-		int cnt = 0;
-
-		int i = 1;
-		while (i <= 5) {
+ 
+		int cnt = 1;
+		while (cnt <= 5) {
 			int num1 = ran.nextInt(8) + 2;
 			int num2 = ran.nextInt(9) + 1;
 			int answer = num1 * num2;
 
 			System.out.print("문제: " + num1 + " * " + num2 + " = ");
 			int myAnswer = sc.nextInt();
-
+			
 			if (answer == myAnswer) {
-				cnt = cnt + 1;
 				System.out.println("정답");
+				System.out.println();
+				score = score + 20;
+			} else {
+				System.out.println("오답");
+				System.out.println();
 			}
-			i++;
+			
+			cnt = cnt + 1;
 		}
-		score = cnt * 20;
-		System.out.println("성적: " + score + "점");
+
+		System.out.println("성적: " + score+"점");
 	}
 
 }
